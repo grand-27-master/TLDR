393619c47583565b6446e048e1540ed48e6dd8e4
Vashishth gajjar <vashishthgajjar@Riteshs-MacBook-Pro.local>
2023-07-18 22:04:26 +0530

    completed

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,45 +7,62 @@ const Demo = () => {
 
     const [article, setArticle] = useState({
         url: '',
-        summaary:''
+        summary:''
     })
 
+    const [allArticles, setAllArticles] = useState([]);
+    const [copied, setCopied] = useState("");
     const [getSummary, { error, isFetching}] = useLazyGetSummaryQuery()
 
+    // Load data from localStorage on mount
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(
+        localStorage.getItem("articles")
+        );
+
+        if (articlesFromLocalStorage) {
+        setAllArticles(articlesFromLocalStorage);
+        }
+    }, []);
+
+
     const handleSubmit = async(e) => {
-        console.log('submit');
+        // console.log('submit');
+
+        e.preventDefault() // prevents page reload
+        const existingArticle = allArticles.find(
+            (item) => item.url === article.url
+        );
+      
+        if (existingArticle)
+        return setArticle(existingArticle);
+
+        const {data}=await getSummary({articleUrl:article.url}) // data is the response from the server
+
+        if(data?.summary){
+            const newArticle={...article,summary:data.summary} // update the state
+            const updatedArticles=[...allArticles,newArticle]
+
+            setArticle(newArticle) 
+            setAllArticles(updatedArticles)
+            localStorage.setItem("articles", JSON.stringify(updatedArticles));
+
+            // console.log(newArticle);
+        }
     }
 
-    // useEffect(() => {
-    //     if (article) {
-    //         fetch('https://api.funtranslations.com/translate/minion.json?text=' + article)
-    //             .then(res => res.json())
-    //             .then(data => {
-    //                 if (data.error) {
-    //                     setError(true)
-    //                     setLoading(false)
-    //                 } else {
-    //                     setResult(data.contents.translated)
-    //                     setLoading(false)
-    //                 }
-    //             })
-    //             .catch(err => {
-    //                 setError(true)
-    //                 setLoading(false)
-    //             })
-    //     }
-    // }, [article])
+    // copy the url and toggle the icon for user feedback
+    const handleCopy = (copyUrl) => {
+        setCopied(copyUrl);
+        navigator.clipboard.writeText(copyUrl);
+        setTimeout(() => setCopied(false), 3000);
+    };
 
-    // const copyToClipboard = () => {
-    //     navigator.clipboard.writeText(result)
-    //     setCopied(true)
-    // }
-
-    // const handleHistory = (text) => {
-    //     setArticle(text)
-    // }
-
-
+    const handleKeyDown = (e) => {
+        if (e.keyCode === 13) {
+        handleSubmit(e);
+        }
+    };
 
   return (
     <section className='mt-16 w-full max-w-xl'>
@@ -54,13 +71,60 @@ const Demo = () => {
             {/* Search Bar */}
             <form className='relative flex justify-center items-center' onSubmit={handleSubmit}>
                 <input type="url" placeholder='🔗 Enter URL' required value={article.url} onChange={(e)=>setArticle({...article,url:e.target.value})} className='url_input peer'/>
-                <button type='submit' className='submit_btn'>📩</button>
+                <button type='submit' className='submit_btn' ><i class='fa fa-send'></i></button>
             </form>
 
             {/* History */}
+            {/* <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                <span>History</span>
+                {allArticles.reverse().map((item, index) => (
+                <div
+                key={`link-${index}`}
+                onClick={() => setArticle(item)}
+                className='link_card'
+                >
+                <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                    <img
+                    src={copied === item.url ? tick : copy}
+                    alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                    className='w-[40%] h-[40%] object-contain'
+                    />
+                </div>
+                <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                    {item.url}
+                </p>
+                </div>
+          ))}
+            </div> */}
         </div>
 
         {/* Result */}
+        <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            An error occured, please try again later
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+        </div>
     </section>
   )
 }
--- src/services/articles.js
+++ src/services/articles.js
@@ -1,20 +1,19 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const axios = require('axios');
-
 const api_key=import.meta.env.VITE_RAPID_API_KEY; 
 
 export const articlesApi = createApi({
     reducerPath: 'articlesApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
     prepareHeaders: (headers) => {
         headers.set('X-RapidAPI-Key', api_key);
         headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
         return headers;
     },
+    }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`, 
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=5`
         })
     })
 });
